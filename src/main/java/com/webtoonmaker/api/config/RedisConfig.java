edb71c1908a57a2e3d1ebe7a7a1282f30e479b6d
“devwonny” <161826579+dev-wonny@users.noreply.github.com>
2025-03-19 18:15:01 +0900

    [Update] environment
    - docker
    - setting.gradle
    - build.gradle
    - application.yml
    - JwtUtil.java
    - RedisConfig.java

--- src/main/java/com/webtoonmaker/api/config/RedisConfig.java
+++ src/main/java/com/webtoonmaker/api/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.webtoonmaker.api.config;
 
+import com.webtoonmaker.api.chat.redis.ChatMessageSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,5 +19,13 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory, ChatMessageSubscriber subscriber) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(subscriber, new PatternTopic("my-channel"));
+        return container;
     }
 }
