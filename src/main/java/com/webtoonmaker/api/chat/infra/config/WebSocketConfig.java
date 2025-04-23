b6bd727fe8214d261cbf8f7016f81a41f427829b
“devwonny” <161826579+dev-wonny@users.noreply.github.com>
2025-04-23 20:29:37 +0900

    [Update] WebSocketConfig 테스트 설정을 위해 변경

--- src/main/java/com/webtoonmaker/api/chat/infra/config/WebSocketConfig.java
+++ src/main/java/com/webtoonmaker/api/chat/infra/config/WebSocketConfig.java
@@ -26,7 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/ws-chat")// 클라이언트가 연결할 endpoint
-            .setAllowedOrigins("*")// CORS 허용
+//            .setAllowedOrigins("*")// CORS 허용
+            .setAllowedOriginPatterns("*")
             .withSockJS();// SockJS 지원
     }
 
@@ -34,14 +35,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 메시지 구독 prefix
-        // 서버 → 클라이언트 전송 prefix, 구독 경로
+        // 서버 → 클라이언트 메세지 전송
+        // prefix, 구독 경로
+        // /topic -> broadcast 용 (채팅방 등 다수 사용자 대상)
+        // /queue -> 1:1 개인 메시지용 (point-to-point)
+        // localhost:8080/ws-chat/topic/chat/{roomId}
         registry.enableSimpleBroker("/topic", "/queue");
 
         // 메시지 전송 prefix
-        // 클라이언트 → 서버 전송 prefix, 메시지 송신 경로 prefix
+        // 클라이언트 → 서버 메시지 전송 prefix
+        // 메시지 송신 경로 prefix
         registry.setApplicationDestinationPrefixes("/app", "/pub");
 
         // convertAndSendToUser 위한 prefix
+        // 특정 사용자에게 보내기 위한 prefix (e.g. 1:1 알림, 귓속말)
+        // localhost:8080/ws-chat/user/queue/messages
         registry.setUserDestinationPrefix("/user");
     }
 }
